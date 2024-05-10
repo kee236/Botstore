ea2fc0fd3a05c02e3df43156fe1e77a8140f8570
kee236 <31869356+kee236@users.noreply.github.com>
2024-05-10 23:57:55 +0700

    Update

---
+++ dashboard.js
@@ -0,0 +1,23 @@
+import { GlAccordionItem } from '@gitlab/ui';
+
+<template>
+  <gl-accordion-item
+    title="Accordion Item Title"
+    titleVisible="Accordion Item Title Expanded"
+    visible
+    :headerLevel="3"
+  >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, maiores.
+  </gl-accordion-item>
+</template>
+
+<template>
+  <gl-accordion-item
+    title="Accordion Item Title"
+    titleVisible="Accordion Item Title Expanded"
+    visible
+    :headerLevel="3"
+  >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, maiores.
+  </gl-accordion-item>
+</template>
